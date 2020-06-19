c87708b9012098bf32472ba0c290413c9a2d6f73
posuna19 <pit19.pol@gmail.com>
2020-06-19 09:11:49 -0400

    updating the messages

--- W2-02-IfElseBlocks.py
+++ W2-02-IfElseBlocks.py
@@ -1,9 +1,9 @@
 
 def validate_name(userName):
     if len(userName) < 3:
-        print('Name is too short. It should be at least 3 characteres')
+        print('Name is too short. It should be at least 3 characteres long')
     elif len(userName) > 8:
-        print('userName is longer. It should be 8 chars long at max')
+        print('userName is longer. It should be 8 chars long at maximum')
     else:
         print('Name is correct')
 
